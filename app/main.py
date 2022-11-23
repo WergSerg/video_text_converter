abe890cf10345274de699b9a9738e153e3700d11
Evgeny Stepanov <stepanov.osv@gmail.com>
2022-11-23 17:39:26 +0300

    Добавил HTML шаблон

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+"""Main app file"""
+
+from fastapi import FastAPI, Request, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+TemplateResponse = Jinja2Templates(directory="app/templates").TemplateResponse
+
+
+@app.post("/convert_video/")
+async def convert_video_file(file: UploadFile):
+    """Convert video file to text and return text"""
+
+    return file.filename
 
 
 @app.get("/")
-def index():
-    return "test"
+async def index(request: Request) -> TemplateResponse:
+    """Index page"""
+
+    return TemplateResponse("index.html", {"request": request})
